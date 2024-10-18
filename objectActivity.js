5273fc9951bb50a873e0c6f345e695999276290e
Valorie Portem <valorieportem@gmail.com>
2024-10-18 14:19:21 +0800

    git commit -m "initial commit"

---
+++ objectActivity.js
@@ -0,0 +1,14 @@
+const car = {
+    type: "Sedan",
+    model: "Accord",
+    color: "Blue"
+  };
+  
+  console.log(typeof car);
+
+  car.type = "Toyota";
+  console.log(car);
+
+  car.wheels = 4;
+  console.log(car); 
+  
